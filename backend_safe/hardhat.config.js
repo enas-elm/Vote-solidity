5c47ec2a8f1538eda1c082c5b51221ebe53c2a33
Enas <enas.elmershati@gmail.com>
2025-06-03 22:15:41 +0200

    feat: add env

--- backend_safe/hardhat.config.js
+++ backend_safe/hardhat.config.js
@@ -1,12 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
   networks: {
     sepolia: {
-      url: "https://sepolia.infura.io/v3/d74f5b383dea457fb9c03ddee73f95d4",
-      accounts: ["36afb17f3dc1519cdc04ca69c3274a7cd66d6aeaf15d3bf82d0f0b4c7ebd6327"]
+      url:`https://sepolia.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.PRIVATE_KEY]
     }
   }
 };
